ea72572899ba61e91d4185c0246119832ee4d2ed
Sathish <iamsathishmohan@outlook.com>
2023-01-02 22:46:24 +0530

    Add project files.

---
+++ WebApplication2/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Data;
+
+namespace WebApplication2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SMDBContext _context;
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger, SMDBContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public async Task OnGet()
+        {
+            var users = await _context.AspNetUsers.Include(x => x.Roles).ToListAsync();
+        }
+    }
+}
